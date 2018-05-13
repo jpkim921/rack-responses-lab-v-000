0947c8441007354004e504e3e487d55d56501948
Jayon Kim <jpkim921@gmail.com>
2018-05-13 18:38:43 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    time = Time.now.localtime("-04:00")
+    # time = Time.now.localtime("-04:00")
+    time - Time.now
 
     if time.hour <=12
       resp.write "Good Morning\n"
